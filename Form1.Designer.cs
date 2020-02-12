07a52a7e43d9a4dbdb8700b66b0f0f403e21288a
Shreya Pandey <shreya.pandey.emip@gmail.com>
2020-02-12 15:34:30 +0530

    Add files via upload
    A C# code for simple task manager. Platform: Visual Studios.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace taskmanager2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listProcess = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnProperties = new System.Windows.Forms.Button();
+            this.btnStartTask = new System.Windows.Forms.Button();
+            this.btnEndTask = new System.Windows.Forms.Button();
+            this.btnGetProcess = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listProcess
+            // 
+            this.listProcess.FormattingEnabled = true;
+            this.listProcess.Location = new System.Drawing.Point(45, 91);
+            this.listProcess.Name = "listProcess";
+            this.listProcess.Size = new System.Drawing.Size(172, 212);
+            this.listProcess.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Task Manager";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnProperties);
+            this.panel1.Controls.Add(this.btnStartTask);
+            this.panel1.Controls.Add(this.btnEndTask);
+            this.panel1.Controls.Add(this.btnGetProcess);
+            this.panel1.Location = new System.Drawing.Point(267, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(173, 212);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnProperties
+            // 
+            this.btnProperties.Location = new System.Drawing.Point(50, 133);
+            this.btnProperties.Name = "btnProperties";
+            this.btnProperties.Size = new System.Drawing.Size(75, 23);
+            this.btnProperties.TabIndex = 3;
+            this.btnProperties.Text = "Properties";
+            this.btnProperties.UseVisualStyleBackColor = true;
+            this.btnProperties.Click += new System.EventHandler(this.btnProperties_Click);
+            // 
+            // btnStartTask
+            // 
+            this.btnStartTask.Location = new System.Drawing.Point(50, 103);
+            this.btnStartTask.Name = "btnStartTask";
+            this.btnStartTask.Size = new System.Drawing.Size(75, 23);
+            this.btnStartTask.TabIndex = 2;
+            this.btnStartTask.Text = "Start Task";
+            this.btnStartTask.UseVisualStyleBackColor = true;
+            this.btnStartTask.Click += new System.EventHandler(this.btnStartTask_Click);
+            // 
+            // btnEndTask
+            // 
+            this.btnEndTask.Location = new System.Drawing.Point(50, 73);
+            this.btnEndTask.Name = "btnEndTask";
+            this.btnEndTask.Size = new System.Drawing.Size(75, 23);
+            this.btnEndTask.TabIndex = 1;
+            this.btnEndTask.Text = "End Task";
+            this.btnEndTask.UseVisualStyleBackColor = true;
+            this.btnEndTask.Click += new System.EventHandler(this.btnEndTask_Click);
+            // 
+            // btnGetProcess
+            // 
+            this.btnGetProcess.Location = new System.Drawing.Point(50, 43);
+            this.btnGetProcess.Name = "btnGetProcess";
+            this.btnGetProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnGetProcess.TabIndex = 0;
+            this.btnGetProcess.Text = "Get Process";
+            this.btnGetProcess.UseVisualStyleBackColor = true;
+            this.btnGetProcess.Click += new System.EventHandler(this.btnGetProcess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 326);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listProcess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnProperties;
+        private System.Windows.Forms.Button btnStartTask;
+        private System.Windows.Forms.Button btnEndTask;
+        private System.Windows.Forms.Button btnGetProcess;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
+
+namespace taskmanager2._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnGetProcess_Click(object sender, EventArgs e)
+        {
+            GetMyProcesses();
+        }
+
+        private void GetMyProcesses()
+        {
+            Process[] task = Process.GetProcesses();
+            foreach (Process proc in task)
+                listProcess.Items.Add(proc.ProcessName);
+        }
+
+        private void btnEndTask_Click(object sender, EventArgs e)
+        {
+            Process[] task = Process.GetProcesses();
+            foreach (Process proc in task)
+                if(listProcess.SelectedItem.ToString()== proc.ProcessName)
+                    
+                {
+                    try
+                    {
+                        proc.Kill();
+                        break;
+                    }
+
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            
+        }
+
+        private void btnStartTask_Click(object sender, EventArgs e)
+        {
+            string path = Microsoft.VisualBasic.Interaction.InputBox("Enter Process Path", "Task Manager", "", 350, 350);
+            Process.Start(path);
+        }
+
+        private void btnProperties_Click(object sender, EventArgs e)
+        {
+            Process[] task = Process.GetProcesses();
+            foreach (Process proc in task)
+                if (listProcess.SelectedItem.ToString() == proc.ProcessName)
+                {
+                    string temp = string.Empty;
+                    temp += "Process ID: " + proc.Id.ToString();
+                    temp += "\nUser Time : " + proc.UserProcessorTime.ToString();
+                    temp += "\nStart Time: " + proc.StartTime.ToString();
+                    MessageBox.Show(temp);
+                    break;
+
+                }
+        }
+    }
+}
